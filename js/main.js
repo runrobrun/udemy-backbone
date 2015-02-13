fcbe04357e7f8a980c9a76d2ae16b96ba0b0d4fe
Rob hammann <runrobrun@gmail.com>
2015-02-13 15:56:45 -0600

    files from section 3

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
+var Vehicle = Backbone.Model.extend({
+    validate: function(attrs) {
+        if (!attrs.registrationNumber){
+            return "registration Number is missing";
+        }
+    },
+    idAttribute: "registrationNumber",
+    urlRoot: "/api/vehicles",
+    start: function() {
+        console.log("Vehicle Started");
+    }
+});
 
-// In the first few sections, we do all the coding here.
-// Later, you'll see how to organize your code into separate
-// files and modules.
+var Car = Vehicle.extend({
+    start: function() {
+        console.log("Car with registration number " + this.get("registrationNumber") + "started");
+    }
+});
+
+var car = new Car({registrationNumber: "XLI887", color: "Blue"});
+
+if (!car.isValid()) {
+    console.log(car.validationError);
+}
